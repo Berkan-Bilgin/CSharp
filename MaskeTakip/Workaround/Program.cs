fd3d805d65fa1dcc0f1b159fe2432a50638191f7
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-02-16 03:40:40 +0300

    Add project files.

---
+++ MaskeTakip/Workaround/Program.cs
@@ -0,0 +1,26 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Person person1 = new Person();
+            person1.FirstName = "ESRA";
+            person1.LastName = "DEMİR";
+            person1.DateOfBirthYear = 1996;
+            person1.NationalIdentity = "123";
+
+            
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+
+        }
+    }
+}
